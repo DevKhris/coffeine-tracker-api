5bf0a25732f4bbaf12db0853d83fc51fc3ce3c7e
Christian H <contact@devkhris.com>
2023-08-29 12:42:40 -0600

    feat: added security headers and csrf

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import helmet from 'helmet';
+import * as csurf from 'csurf';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: NestApplication = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
+  app.use(csurf());
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
